f887e2c0d46f0ac30e37ec4e0e4a62c5da12ba1c
NASIR ABBAS <85347981+nasirabbas827@users.noreply.github.com>
2026-06-05 11:48:24 +0500

    security: remove exposed credentials

--- Electronic_Voting_system/settings.py
+++ Electronic_Voting_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x_jin-#!!r7-_&g9)v(eenyh5-vrg6vlq7pd^vsriyn8kdjl2c'
+SECRET_KEY = "YOUR_OWN_API_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
